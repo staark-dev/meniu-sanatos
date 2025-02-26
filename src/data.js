a6229cdc1f35a41ff08ef2466e96c389996b334b
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2025-02-26 23:19:54 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,6 +1,9 @@
 export const fetchPlanAlimentar = async () => {
   try {
     const response = await fetch("/plan_alimentar.json");
+    if (!response.ok) {
+      throw new Error(`Eroare la încărcarea JSON: ${response.status} ${response.statusText}`);
+    }
     const data = await response.json();
     return data;
   } catch (error) {
