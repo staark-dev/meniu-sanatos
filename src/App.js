eca0ce237972bf0b39f6780058e020f747d5dd8e
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2025-02-27 00:34:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,20 +13,6 @@ const App = () => {
   const [popupMessage, setPopupMessage] = useState("");
   const [weeklyProgress, setWeeklyProgress] = useState(0);
 
-const calculateWeeklyProgress = () => {
-  let totalTasks = 0;
-  let completedTasksTotal = 0;
-
-  Object.values(plan[selectedWeek].days).forEach(day => {
-    if (day.tasks) {
-      totalTasks += day.tasks.length;
-      completedTasksTotal += day.tasks.filter(task => task.completed).length;
-    }
-  });
-
-  return totalTasks > 0 ? (completedTasksTotal / totalTasks) * 100 : 0;
-};
-
 useEffect(() => {
   fetchPlanAlimentar().then((data) => {
     if (data) {
@@ -42,6 +28,20 @@ useEffect(() => {
     }
   });
 }, []);
+
+const calculateWeeklyProgress = () => {
+  let totalTasks = 0;
+  let completedTasksTotal = 0;
+
+  Object.values(plan[selectedWeek].days).forEach(day => {
+    if (day.tasks) {
+      totalTasks += day.tasks.length;
+      completedTasksTotal += day.tasks.filter(task => task.completed).length;
+    }
+  });
+
+  return totalTasks > 0 ? (completedTasksTotal / totalTasks) * 100 : 0;
+};
 
 useEffect(() => {
   setWeeklyProgress(calculateWeeklyProgress());
