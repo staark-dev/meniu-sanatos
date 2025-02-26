472d0f0681cbc73d2c3af2069236191e8164ee95
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2025-02-26 13:06:58 +0100

    Update src

---
+++ src/data.js
@@ -0,0 +1,13 @@
+const menuData = {
+  "week_1": {
+    "monday": {
+      "breakfast": "Omletă cu legume",
+      "lunch": "Piept de pui cu orez brun",
+      "dinner": "Somon la cuptor",
+      "snacks": ["Iaurt grecesc", "Un măr"],
+      "water": "2L"
+    }
+  }
+};
+
+export default menuData;
