8e5d07cc19eaea72cc5c49cda057a291e8fb49b3
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2025-02-26 22:16:39 +0100

    Update tasks.js

--- src/tasks.js
+++ src/tasks.js
@@ -1,14 +1,52 @@
 const tasksData = {
-  week_1: [
-    { id: 1, text: "Bea 2L apă", completed: false },
-    { id: 2, text: "Fă 30 min de mișcare", completed: false },
-    { id: 3, text: "Mănâncă fructe și legume", completed: false }
-  ],
-  week_2: [
-    { id: 4, text: "Bea ceai verde dimineața", completed: false },
-    { id: 5, text: "Evită zahărul procesat", completed: false },
-    { id: 6, text: "Odihnește-te 8 ore", completed: false }
-  ]
+  week_1: {
+    monday: [
+      { id: 1, text: "Bea 2L apă", completed: false },
+      { id: 2, text: "Fă 30 min de mișcare", completed: false },
+      { id: 3, text: "Mănâncă fructe și legume", completed: false }
+    ],
+    tuesday: [
+      { id: 4, text: "Bea ceai verde dimineața", completed: false },
+      { id: 5, text: "Evită zahărul procesat", completed: false },
+      { id: 6, text: "Odihnește-te 8 ore", completed: false }
+    ]
+  },
+  week_2: {
+    monday: [
+      { id: 7, text: "Consumă proteine la fiecare masă", completed: false },
+      { id: 8, text: "Fă stretching 10 minute", completed: false },
+      { id: 9, text: "Redu consumul de sare", completed: false }
+    ], 
+    tuesday: [
+      { id: 4, text: "Bea ceai verde dimineața", completed: false },
+      { id: 5, text: "Evită zahărul procesat", completed: false },
+      { id: 6, text: "Odihnește-te 8 ore", completed: false }
+    ]
+  }, 
+  week_3: {
+    monday: [
+      { id: 7, text: "Consumă proteine la fiecare masă", completed: false },
+      { id: 8, text: "Fă stretching 10 minute", completed: false },
+      { id: 9, text: "Redu consumul de sare", completed: false }
+    ], 
+    tuesday: [
+      { id: 4, text: "Bea ceai verde dimineața", completed: false },
+      { id: 5, text: "Evită zahărul procesat", completed: false },
+      { id: 6, text: "Odihnește-te 8 ore", completed: false }
+    ]
+  },
+  week_4: {
+    monday: [
+      { id: 7, text: "Consumă proteine la fiecare masă", completed: false },
+      { id: 8, text: "Fă stretching 10 minute", completed: false },
+      { id: 9, text: "Redu consumul de sare", completed: false }
+    ], 
+    tuesday: [
+      { id: 4, text: "Bea ceai verde dimineața", completed: false },
+      { id: 5, text: "Evită zahărul procesat", completed: false },
+      { id: 6, text: "Odihnește-te 8 ore", completed: false }
+    ]
+  }
 };
 
 export default tasksData;
